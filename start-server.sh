629663fe88acfffe89243bdca8cab7a4e638c5c3
nikolai <n.ivanov.p@gmail.com>
2017-04-22 16:16:16 +0300

    updated docker-compose.yml

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
+wait_for_db() {
+  nslookup mongodb
+  if ! nc -z mongodb 27017; then
+    echo "Waiting for mongodb..."
+    sleep 2
+    wait_for_db
+  fi
+}
+
+wait_for_db
+
 node app.js
